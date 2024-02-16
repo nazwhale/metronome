9ba026a124e6f78c30a3bdd702e7b8991dbee6cb
Naz Malik <nazmalik08@gmail.com>
2024-02-16 14:29:03 +0000

    use index

--- src/RandomPromptGenerator/index.tsx
+++ src/RandomPromptGenerator/index.tsx
@@ -6,7 +6,9 @@ import { useEffect, useState } from "react";
 import { prompts } from "./prompts";
 
 const RandomPromptGenerator = () => {
-  const [activePrompt, setActivePrompt] = useState<string | null>(null);
+  const [activePromptIndex, setActivePromptIndex] = useState<number | null>(
+    null,
+  );
 
   // on mount, generate a random prompt
   useEffect(() => {
@@ -14,20 +16,26 @@ const RandomPromptGenerator = () => {
   }, []);
 
   const generateRandomPrompt = () => {
-    const randomIndex = Math.floor(Math.random() * prompts.length);
-    setActivePrompt(prompts[randomIndex]);
+    let randomIndex = Math.floor(Math.random() * prompts.length);
+
+    // If randomIndex is the same as the current activePromptIndex, try again
+    if (randomIndex === activePromptIndex) {
+      randomIndex = Math.floor(Math.random() * prompts.length);
+    }
+
+    setActivePromptIndex(randomIndex);
   };
 
   return (
     <div className="p-4 max-w-md mx-auto">
       <div className="min-h-[8rem] flex items-center justify-center card shadow bg-base-100 bg-neutral-content">
-        {activePrompt && (
-          <p className="text-xl font-semibold">{activePrompt}</p>
+        {activePromptIndex != null && (
+          <p className="text-xl font-semibold">{prompts[activePromptIndex]}</p>
         )}
       </div>
       <div className="my-6">
         <button className="btn btn-primary" onClick={generateRandomPrompt}>
-          Next prompt
+          Random prompt
         </button>
       </div>
 
