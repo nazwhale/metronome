455758c3f8aea7270f0ed6d25de522923058844c
Naz Malik <nazmalik08@gmail.com>
2026-02-03 10:25:07 +0000

    .

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import All from "./All.tsx";
 
 const Navigation: React.FC = () => {
   const location = useLocation();
+  const detailsRef = useRef<HTMLDetailsElement>(null);
+
+  const closeDropdown = () => {
+    if (detailsRef.current) {
+      detailsRef.current.removeAttribute("open");
+    }
+  };
 
   return (
     <nav>
       <div className="flex-none">
         <ul className="menu menu-horizontal">
           <li>
-            <details>
+            <details ref={detailsRef}>
               <summary>tools</summary>
               <ul className="p-2 bg-base-100 rounded-t-none z-50 end-0 w-max">
                 {All.map((metronome) => (
                   <li key={metronome.path} className="list-none w-100%">
                     <Link
                       to={metronome.path}
+                      onClick={closeDropdown}
                       className={`link link-hover ${
                         location.pathname === metronome.path
                           ? "link-success"
